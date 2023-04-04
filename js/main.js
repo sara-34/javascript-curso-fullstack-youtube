52fd9296cf5e0266802c3edde3e2e1c6b71cf7a8
sara-34 <saritaariaslondono12@gmail.com>
2023-04-04 13:44:05 -0500

    Actividades js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let hugo = 15;
+let paco = 2 * hugo;
+let luis = paco + 10;
+
+let total = hugo + paco + luis;
+
+if (total === 85) {
+    console.log("Hugo: " + hugo);
+    console.log("Paco: " + paco);
+    console.log("Luis: " + luis);
+} else{
+    console.log("La cantidad de monedas que hay es: " + total);
+}
+
+
+//Solucion segundo ejercicio.
+
+typeof obj === "object";
+typeof obj === "object" && obj != null;
+
+// Funcion
+
+let name = "Sara";
+let nickname = name;
+name = "Motas"
